99d2cf48ce7566850bf4bdd11afaac477f8af9f6
Varun Ravishankar <vrdabomb@gmail.com>
2012-05-14 17:39:01 -0400

    Began adding unit tests.
    Signed-off-by: Varun Ravishankar <vrdabomb@gmail.com>

---
+++ test_gen_misspelled.py
@@ -0,0 +1,24 @@
+import unittest
+from gen_misspelled import *
+
+
+class TestGenMisspelled(unittest.Testcase):
+    """docstring for TestGenMisspelled"""
+
+    def test_load_dict(dict_path):
+        pass
+
+    def test_misspell(words):
+        pass
+
+    def test_generate_case_error(word):
+        pass
+
+    def test_generate_vowel_error(word):
+        pass
+
+    def test_generate_repeat_error(word):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
